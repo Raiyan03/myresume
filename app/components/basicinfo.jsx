0a8e320d9a38b4d4a5f5a7298585f8494b6d49a0
Ryan <ryansheikh003@gmail.com>
2024-07-26 19:43:15 -0600

    Made the pages mobile responsive

--- app/components/basicinfo.jsx
+++ app/components/basicinfo.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 import Skills from "./skills";
 import { CiLinkedin } from "react-icons/ci";
@@ -7,25 +7,32 @@ import Education from "./education";
 
 const BasicInfo = () => {
   return (
-    <div className=" p-5 h-fit border shadow-lg rounded-xl flex gap-2">
-        <Image src="/image.png" width={150} height={150} className=" rounded-full" />
-        <div className="text-xl p-2 flex flex-col gap-1">
-          <span className="font-bold">Raiyna Anwar</span>
-          <span>Software developer</span>
-          <span className=" font-extralight ">MERN|Next.js</span>
-          <div className="flex gap-3">
-            <Link href="https://www.linkedin.com/in/raiyan-anwar/">
-                <CiLinkedin size={30} />
-            </Link>
-            <Link href="https://github.com/Raiyan03" >
-                <FaGithub size={30} />
-            </Link>
-          </div>
+    <div className="p-5 border shadow-lg rounded-xl flex flex-col md:flex-row gap-2">
+      <Image
+        src="/image.png"
+        width={150}
+        height={150}
+        className="rounded-full self-center md:self-start"
+      />
+      <div className="text-xl p-2 flex flex-col gap-1">
+        <span className="font-bold">Raiyna Anwar</span>
+        <span>Software Developer</span>
+        <span className="font-extralight">MERN | Next.js</span>
+        <div className="flex gap-3">
+          <Link href="https://www.linkedin.com/in/raiyan-anwar/">
+            <CiLinkedin size={30} />
+          </Link>
+          <Link href="https://github.com/Raiyan03">
+            <FaGithub size={30} />
+          </Link>
         </div>
-        <Skills/>
+      </div>
+      <div className="flex flex-col md:flex-row gap-2">
+        <Skills />
         <Education />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BasicInfo
+export default BasicInfo;
--- app/components/skills.jsx
+++ app/components/skills.jsx
@@ -1,6 +1,6 @@
 const Skills = () => {
     return (
-        <div className=" flex flex-col p-2 ml-5 text-xl">
+        <div className=" flex flex-col p-2 text-xl">
             <h1 className="font-bold">Skills</h1>
             <p>Languages: Js, Java, Python, C# & Rust </p>
             <p>Tools: Git, GitHub, Azure & Docker</p>
